e3dba6859994532d6274b137e978f8f04870c02d
Subhash Gopalakrishnan <subhashgo@gmail.com>
2013-09-23 08:47:03 +0530

    Initial commit

---
+++ app/src/pedestal_quiz/html_templates.clj
@@ -0,0 +1,19 @@
+(ns pedestal-quiz.html-templates
+  (:use [io.pedestal.app.templates :only [tfn dtfn tnodes]]))
+
+(defmacro pedestal-quiz-templates
+  []
+  ;; Extract the 'hello' template from the template file pedestal-quiz.html.
+  ;; The 'dtfn' function will create a dynamic template which can be
+  ;; updated after it has been attached to the DOM.
+  ;;
+  ;; To see how this template is used, refer to
+  ;;
+  ;; app/src/pedestal_quiz/rendering.cljs
+  ;;
+  ;; The last argument to 'dtfn' is a set of fields that should be
+  ;; treated as static fields (may only be set once). Dynamic templates
+  ;; use ids to set values so you cannot dynamically set an id.
+  {:pedestal-quiz-page (dtfn (tnodes "pedestal-quiz.html" "hello") #{:id})})
+
+;; Note: this file will not be reloaded automatically when it is changed.
---
+++ app/src/pedestal_quiz/simulated/services.cljs
@@ -0,0 +1,3 @@
+(ns pedestal-quiz.simulated.services)
+
+;; Implement services to simulate talking to back-end services
